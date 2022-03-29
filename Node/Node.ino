78e0dee4f2c5f4bf9f23969fc6d6caea1a863ae4
Matteo Vidali <mvidali@iu.edu>
2022-03-29 15:44:00 -0400

    Server is now working

---
+++ Node/Node.ino
@@ -0,0 +1,136 @@
+#include <Servo.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+
+#ifndef STASSID
+#define STASSID "Leo's Den"
+#define STAPSK "blueflute165"
+#endif
+
+const char* ssid = STASSID;
+const char* pass = STAPSK;
+const uint8_t servoPin = 5;
+const int led = 13;
+
+int httpCode;
+int floor_code;
+String payload;
+
+Servo servo;
+ESP8266WebServer server(80);
+HTTPClient http;
+WiFiClient wifi;
+
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "root\r\n");
+  digitalWrite(led, 0);
+}
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void reset_servo(void){
+  servo.write(90);
+}
+
+void press_button(int floor){
+  Serial.print("BUTTON CALLED\n");
+  servo.write(floor*180);
+  delay(100);
+  reset_servo();
+}
+
+void press_up(){
+  server.send(200, "text/plain", "Going Up!\r\n");
+  press_button(1);
+  delay(1000);
+}
+void press_down(){
+  server.send(200, "text/plain", "Going Down!\r\n");
+  press_button(0);
+  delay(1000);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+  server.on("/press1", press_up);
+  server.on("/press0", press_down);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  http.begin(wifi, "http://ec2-18-219-245-250.us-east-2.compute.amazonaws.com/getCall");
+  Serial.println("HTTP server started");
+  servo.attach(servoPin);
+  servo.write(90);
+  delay(5000);
+
+}
+
+void loop() {
+  server.handleClient();
+  MDNS.update();
+  httpCode = http.GET();
+  if (httpCode > 0){
+    Serial.print("HTTP CODE:");
+    Serial.println(httpCode);
+    payload = http.getString();
+    Serial.println(payload);
+    floor_code = payload.toInt();
+    if (floor_code == -1){
+      Serial.println("No Waiting calls");
+    }
+    if(floor_code == 0){
+      Serial.println("DOWN BUTTON PRESSED");
+      press_down();
+    }
+    if(floor_code == 1){
+      Serial.println("UP BUTTON PRESSED");
+      press_up();
+    }
+    else{
+      Serial.print("FLOOR ");
+      Serial.println(floor_code);
+    }
+  }
+  delay(5000);
+}
--- Server/Elevate_Server.py
+++ Server/Elevate_Server.py
@@ -1,3 +1,5 @@
+from ast import Call
+from sre_constants import CALL
 from flask import Flask
 from flask import jsonify
 
@@ -7,18 +9,38 @@ def print_hello():
     return 'Hello World'
 
 def call_floor(node_id, floor):
+    if not node_id.isnumeric():
+        return 'Node ID must be a number'
+    if int(node_id) != 1:
+        return 'Not reconized node id'
+    if not floor.isnumeric():
+        if floor.lower() == 'up':
+            floor = 1
+        elif floor.lower() == 'down':
+            floor = 0
+        else:
+            return f'"{floor.lower()}" is not a recognized floor'
+    
     CALL_LIST.append((node_id, floor))
     print(CALL_LIST)
-    return f'Calling floor {floor} @ node {node_id}'
+    return f'Calling {floor} @ node {node_id}'
 
 def check_list():
     return jsonify(CALL_LIST)
+
+def get_next_call():
+    if len(CALL_LIST) == 0:
+        return '-1'
+    call = f'{CALL_LIST[0][1]}'
+    CALL_LIST.pop(0)
+    return call
 
 server = Flask(__name__)
 
 server.add_url_rule('/', 'index', print_hello)
 server.add_url_rule('/<node_id>/<floor>', 'floor_call', call_floor)
 server.add_url_rule('/List', 'call_list', check_list)
+server.add_url_rule('/getCall', 'get-next-call', get_next_call)
 
 if __name__ == '__main__':
     server.debug = True
